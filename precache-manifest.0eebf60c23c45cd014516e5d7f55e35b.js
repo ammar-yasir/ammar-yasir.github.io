9f5990959163db18a18569a9d400e8762373bc05
Muhammad Ammar Yasir <ammaryasir3011@gmail.com>
2020-10-29 01:16:49 +0500

    Updates

---
+++ precache-manifest.0eebf60c23c45cd014516e5d7f55e35b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4fcc204dbc89991a5b9241d801bf678",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3215eee3aa96f6673a93",
+    "url": "/portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0a226e69c1d7b8a3ed9c",
+    "url": "/portfolio/static/js/2.cc84dc99.chunk.js"
+  },
+  {
+    "revision": "3215eee3aa96f6673a93",
+    "url": "/portfolio/static/js/main.82f454e7.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "8a396670f9169263baeb071880a740f3",
+    "url": "/portfolio/static/media/profile.8a396670.jpg"
+  }
+]);
