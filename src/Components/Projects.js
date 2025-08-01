755f40e6c7365939d7f83110aaf8f024370071a5
Muhammad Ammar Yasir <ammaryasir3011@gmail.com>
2025-08-01 14:28:32 +0500

    fix: remove dates from projects list

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import dayjs from 'dayjs';
+// import dayjs from 'dayjs';
 import { useContentful } from 'react-contentful';
 
 const Projects = () => {
@@ -22,12 +22,12 @@ const Projects = () => {
                 </a>
                 <div className="subheading mb-3">{item.fields?.shortDescription}</div>
               </div>
-              <div className="resume-date text-md-right">
+              {/* <div className="resume-date text-md-right">
                 <span className="text-primary">
                   {dayjs(item?.fields?.startDate).format('MMM, YYYY')} - {''}
                   {item?.fields?.endDate ? dayjs(item?.fields?.endDate).format('MMM, YYYY') : 'Present'}
                 </span>
-              </div>
+              </div> */}
             </div>
           ))
         }
